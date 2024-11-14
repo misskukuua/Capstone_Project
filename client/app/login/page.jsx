2ad779f59009fdba4d93da726fe22a208536bd97
misskukuua <p.asare@alustudent.com>
2024-11-14 09:23:31 -0800

    corrected the component suspense error

--- client/app/login/page.jsx
+++ client/app/login/page.jsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import { useState, useEffect, Suspense } from 'react'
 import { Input } from "@/components/ui/input"
@@ -11,16 +11,14 @@ export default function LoginPage() {
   const [formData, setFormData] = useState({ email: '', password: '' })
   const [error, setError] = useState('')
   const router = useRouter()
+  const searchParams = useSearchParams()
 
-  
-  // const searchParams = useSearchParams()
-
-  // Wrap the searchParams hook usage in Suspense
-  const searchParams = (
-    <Suspense fallback={<div>Loading...</div>}>
-      {useSearchParams()}
-    </Suspense>
-  );
+  // // Wrap the searchParams hook usage in Suspense
+  // const searchParams = (
+  //   <Suspense fallback={<div>Loading...</div>}>
+  //     {useSearchParams()}
+  //   </Suspense>
+  // );
 
 
   // Handle input changes
@@ -76,7 +74,7 @@ export default function LoginPage() {
     }
   }
   const handleGoogleLogin = () => {
-    window.location.href = 'http://localhost:5001/api/auth/google'
+    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/api/auth/google`;
   }
 
 
@@ -161,7 +159,7 @@ export default function LoginPage() {
             <path
               fill="#1976D2"
               d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
-              onClick={() => window.location.href = 'http://localhost:5001/api/auth/google'}
+              onClick={() => window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/api/auth/google`}
 
             />
           </svg>
