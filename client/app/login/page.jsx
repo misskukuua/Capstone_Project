bf544f8973c604195c57f139bd9fe668ecc76014
misskukuua <p.asare@alustudent.com>
2024-11-13 18:25:11 -0800

    wapped the component with suspense

--- client/app/login/page.jsx
+++ client/app/login/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Suspense } from 'react'
 import { Input } from "@/components/ui/input"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Button } from "@/components/ui/button"
